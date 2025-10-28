728a3c01bdf7200f52aa67e4e989df74db527469
bunraksa0106 <Bunraksastudent9d@gmail.com>
2025-10-28 13:20:15 +0700

    first commit

--- style.js
+++ style.js
@@ -7,19 +7,13 @@ window.closeModalM = function () {
     if (!viewAll) return;
         viewAll.classList.add('hidden');
 };
-// head.style.position="fixed";
-// head.style.width="100%";
-// head.style.height="25vh";
-// head.style.backgroundColor="white";
-// head.style.marginTop="0";
 
 const head = document.getElementById('head');
-    //   window.addEventListener('scroll', function () {
-    //     if (window.pageYOffset >= 0&&head) {
-    //       head.style.position = 'fixed';
-          head.style.top = '0';
-    //       head.style.width = '100%';
-    //       head.style.height='20vh';
-
-    //     }
-    // });
+  head.style.top = '0';
+const btn = document.getElementById('btn');
+  btn.addEventListener("click", () => {
+    btn.textContent = "Hide";
+  });
+  btn.addEventListener("onmuseout", () => {
+    return ;
+  });
